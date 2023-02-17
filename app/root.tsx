a9cd16b695387752f58d9eb4ef14a20fe8c2f62d
Stephen Chung <whosteev95@gmail.com>
2023-02-16 20:36:04 -0500

    ui kit

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,22 @@
 import { Link, LiveReload, Outlet } from "@remix-run/react";
+import { extractStyles } from "evergreen-ui";
 
 export default function App() {
+  const { css, hydrationScript } = extractStyles();
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
+        <title>Drip</title>
+
+        {/* https://github.com/segmentio/evergreen/issues/154#issuecomment-375766412 */}
+        <style id="evergreen-css" dangerouslySetInnerHTML={{ __html: css }} />
+        {hydrationScript}
+
+        <Link to="/">Home</Link>
       </head>
       <body>
-        <Link to="/">Home</Link>
-
         <LiveReload />
-        {/* this lays out pages in /routes */}
         <Outlet />
       </body>
     </html>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,9 +2,12 @@ import { ActionArgs, redirect } from "@remix-run/node";
 import { db } from "~/backend/db.server";
 
 import randomWords from "random-words";
+import { Form } from "@remix-run/react";
+import { Button, Label, Pane, TextInput } from "evergreen-ui";
 
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData();
+
   const content = form.get("content");
   // we do this type check to be extra sure and to make TypeScript happy
   // we'll explore validation next!
@@ -12,31 +15,28 @@ export const action = async ({ request }: ActionArgs) => {
     throw new Error(`Form not submitted correctly.`);
   }
 
-  // TODO: generate random room names
+  const message = await db.message.create({
+    data: { content, room: randomWords(2).join("-") },
+  });
 
-  const fields = { content, room: randomWords(5).join("-") };
-
-  const message = await db.message.create({ data: fields });
-  console.log({ message });
   return redirect(`/drip/${message.room}`);
 };
 
 export default function IndexRoute() {
   return (
-    <div>
-      <h1>Need to drip?</h1>
-      {/* ?index is needed to make action work at root .. */}
-      <form method="post" action="/?index">
-        <div>
-          <label>
-            Content:
-            <input type="text" name="content" />
-          </label>
-        </div>
-        <button type="submit" className="button">
-          New Drip
-        </button>
-      </form>
-    </div>
+    <Pane width="100%">
+      <h1>Drip</h1>
+      <Pane display="flex" justifyContent="center">
+        <Form method="post">
+          <Label marginRight="12px" htmlFor="content">
+            Message
+          </Label>
+          <TextInput type="text" name="content" />
+          <Button marginLeft="12px" type="submit">
+            Send message to start a new room
+          </Button>
+        </Form>
+      </Pane>
+    </Pane>
   );
 }
