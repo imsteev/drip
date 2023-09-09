7d5d1d188bca02a4d42024667f1d59aa1dfe1280
Stephen Chung <whosteev95@gmail.com>
2023-09-09 12:27:11 -0400

    simplify

--- cmd/controller.go
+++ cmd/controller.go
@@ -45,22 +45,11 @@ func (c *Controller) GetSpace(w http.ResponseWriter, r *http.Request) {
 
 func (c *Controller) NewSpace(w http.ResponseWriter, r *http.Request) {
 	newSpaceID := strconv.Itoa(rand.Int())
-	c.Store.AddSpace(newSpaceID)
-	msgs, err := c.Store.FindMessages(data.SpaceID(newSpaceID))
-	if err != nil {
-		utils.WriteStrf(w, "error generating template: %v", err)
-	}
-	strs := []string{}
-	for _, m := range msgs {
-		strs = append(strs, m.Message)
-	}
 	tmpl := templates.Index{
-		Messages: strs,
-		RoomURL:  BASE_URL + "/spaces/" + newSpaceID,
-		Space:    newSpaceID,
+		RoomURL: BASE_URL + "/spaces/" + newSpaceID,
+		Space:   newSpaceID,
 	}
 	w.Header().Add("HX-Push-Url", "/spaces/"+newSpaceID)
-
 	tmpl.MustRender(w)
 }
 
